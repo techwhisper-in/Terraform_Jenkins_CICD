6b3e8b498b65c5a3aa2b3b61f072c807bf2026fe
NeeharikaRN <n.neeharikareddy@gmail.com>
2024-07-22 12:01:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,13 @@ resource "aws_s3_bucket" "bucket2" {
 provider "aws" {
     region = "eu-west-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "neeha-s3bucket-date"
+    key            = "globalstate/s3/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "neeha-date-dblocks"
+    encrypt        = true
+  }
+}
